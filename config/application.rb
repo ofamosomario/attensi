0340e6748469493a155e9e490b73bd1c9affb18e
mario <tkyakow@gmail.com>
2021-05-14 17:42:09 -0300

    assets.

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Quizgame
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false 
 
     # Configuration for the application, engines, and railties goes here.
     #
